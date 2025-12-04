0b9022aac0e83aa42f3653c351780a938a2a7676
digambar <digambarb18@gmail.com>
2025-12-04 22:39:16 +0530

    day 1 js

---
+++ index.js
@@ -0,0 +1,12 @@
+// variables and constants 
+
+let name="diagmbar "
+ 
+const gmail="digambarb18@gmail.com"
+
+var collage="indira collage "
+
+let course=true;
+
+
+console.table([name,gmail,collage,course]) // can use to console multiple values at once 
